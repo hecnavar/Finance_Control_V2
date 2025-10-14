a81c8f11fced50ced406d974210885d1f1514ea1
hecnavar <hmnm0007@gmail.com>
2025-10-13 19:55:29 -0600

    Feat: First commit

---
+++ src/test/java/Bedu/Project/Finance_Control/FinanceControlApplicationTests.java
@@ -0,0 +1,13 @@
+package Bedu.Project.Finance_Control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
